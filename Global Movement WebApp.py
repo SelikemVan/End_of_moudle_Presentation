c3dcb50960e4fa5b893963b9002276792b825362
Selikem Kofi Van Agbemafo <138385228+SelikemVan@users.noreply.github.com>
2023-09-02 00:02:44 +0100

    second commit of End of Module Presentation

---
+++ Global Movement WebApp.py
@@ -0,0 +1,66 @@
+print("Welcome to Vankem's Global Movement App!")
+
+
+def send_confirmation_email():
+    pass
+
+
+def send_confirmation_sms():
+    pass
+
+
+def registration(name, postal_address, email_address, phone_number, password):
+    """
+  This function registers a new user on the Global Movement app.
+
+  Args:
+    name: The user's name.
+    postal_address: The user's postal address.
+    email_address: The user's email address.
+    phone_number: The user's phone number.
+    password: The user's password.
+
+  Returns:
+    A boolean value indicating whether the registration was successful.
+    """
+
+    if not name or not postal_address or not email_address or not phone_number:
+        return False
+
+    # Create a dictionary to store the user data.
+    user_data = {
+        'name': name,
+        'postal_address': postal_address,
+        'email_address': email_address,
+        'phone_number': phone_number,
+        'password': password
+    }
+
+    # Add the user data to the list of users.
+    users.append(user_data)
+
+    # Send a confirmation email and SMS message to the user.
+    send_confirmation_email()
+    send_confirmation_sms()
+
+    return True
+
+
+def login(email_address, password):
+    """
+  This function logs in a user to the Global Movement app.
+
+  Args:
+    email_address: The user's email address.
+    password: The user's password.
+
+  Returns:
+    A boolean value indicating whether the login was successful.
+  """
+
+    # Check if the user exists in the list of users.
+    for user in users:
+        if user['email_address'] == email_address and user['password'] == hash(password):
+            return user['session_id']
+
+    return False
