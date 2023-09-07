05bc36ee0297711345517a61732ff84433d05d3b
Selikem Kofi Van Agbemafo <138385228+SelikemVan@users.noreply.github.com>
2023-09-07 20:45:21 +0100

    fourth commit of End of Module Presentation

--- Global Movement WebApp.py
+++ Global Movement WebApp.py
@@ -45,13 +45,87 @@ def login(users):
     print("Invalid email or password.")
 
 
-# Main program
+def payment_options():
+    print("Select your payment method:")
+    print("1. Credit card")
+    print("2. Debit card")
+    print("3. PayPal")
+    payment_method = input("Enter your choice: ")
+    print("You have selected {} as your payment method.".format(payment_method))
+    return payment_method
+
+
+def transport_services():
+    print("Global Movement App")
+    print("Transport services")
+    print("1. Luxurious bus services \n2. Airplane services\n3. Train services\n4. Taxi/Metro Services\n5. Hotel "
+          "services")
+    trs = input("Enter your choice: ")
+    print("You have selected {} transport service.".format(trs))
+
+    if trs == "1":
+        print("Enter your desired luxury bus location from:\nAccra\nKumasi\nTakoradi\nTamale\nHo\nCape-Coast\n: ")
+        origin = input()
+        print("Enter your desired luxury bus location to: ")
+        destination = input()
+        print("You have selected to travel from {} to {}".format(origin, destination))
+
+        adults = int(input("Enter the number of adults: "))
+        children = int(input("Enter the number of children: "))
+        basic_fare = 1000 * adults + 500 * children
+        discount = 0.2 * basic_fare if adults > 3 else 0
+        fare = basic_fare - discount
+        print("The fare for the luxurious bus is {}.".format(fare))
+
+        if trs == "2":
+            print("Enter your desired airplane location from:\nAccra\nKumasi\nTakoradi\nTamale\nHo\nCape-Coast\n: ")
+            origin = input()
+            print("Enter your desired airplane location to: ")
+            destination = input()
+            print("You have selected to travel from {} to {}".format(origin, destination))
+
+            adults = int(input("Enter the number of adults: "))
+            children = int(input("Enter the number of children: "))
+            basic_fare = 1000 * adults + 500 * children
+            discount = 0.5 * basic_fare if adults > 3 else 0
+            fare = basic_fare - discount
+            print("The fare for the airplane is {}.".format(fare))
+
+        if trs == "3":
+            print("Enter your desired Train location from:\nAccra\nKumasi\nTakoradi\nTamale\nHo\nCape-Coast\n: ")
+            origin = input()
+            print("Enter your desired Train location to: ")
+            destination = input()
+            print("You have selected to travel from {} to {}".format(origin, destination))
+
+        if trs == "4":
+            print("Enter your desired Taxi/Metro location from:\nAccra\nKumasi\nTakoradi\nTamale\nHo\nCape-Coast\n: ")
+            origin = input()
+            print("Enter your desired Taxi/Metro location to: ")
+            destination = input()
+            print("You have selected to travel from {} to {}".format(origin, destination))
+
+        if trs == "5":
+            print("Do you want to book a hotel? (yes/no)")
+            hotel_choice = input()
+
+            if hotel_choice == "yes":
+                print("Enter the hotel name: ")
+                hotel_name = input()
+                print("You have selected to book a hotel in {}".format(hotel_name))
+
+            if hotel_choice == "no":
+                return
+
+                # Main program
+
+
 users = []
 
 print("Welcome to the main Application")
 
 while True:
-    choice = input("Choose an option:\n1. Register\n2. Login\n3. Exit\n")
+    choice = input("Choose an option:\n1. Register\n2. Login\n3. Exit\n4. Payment\n")
 
     if choice == "1":
         registration(users)
@@ -63,7 +137,9 @@ while True:
         print("Goodbye!")
         break
 
+    if choice == "4":
+        payment_method1 = payment_options()
+        transport_services()
+
     else:
         print("Invalid choice!")
-
-        
